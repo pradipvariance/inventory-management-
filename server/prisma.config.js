07029984549d44ecdf9eb338b164008324f9ca1b
pawarjayvariance <jay.pawar@varianceinfotech.com>
2026-02-18 12:37:20 +0530

    prisma error solved

---
+++ server/prisma.config.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+import { defineConfig } from '@prisma/config';
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
