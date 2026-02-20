e18bfc295ea9319b6c492c750cd6db934c514ef1
pawarjayvariance <jay.pawar@varianceinfotech.com>
2026-02-20 10:18:51 +0530

    commit

--- server/prisma/migrations/20260218060819_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "amount" DECIMAL(65,30) NOT NULL DEFAULT 0.0,
-ADD COLUMN     "image" TEXT;
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "warehouseId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_warehouseId_fkey" FOREIGN KEY ("warehouseId") REFERENCES "Warehouse"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- server/prisma/migrations/20260219050007_link_po_to_user_supplier/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "PurchaseOrder" DROP CONSTRAINT "PurchaseOrder_supplierId_fkey";
-
--- AddForeignKey
-ALTER TABLE "PurchaseOrder" ADD CONSTRAINT "PurchaseOrder_supplierId_fkey" FOREIGN KEY ("supplierId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
